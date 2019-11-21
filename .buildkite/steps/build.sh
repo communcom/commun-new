c328ac461ca3cda3624d9001976919de6a5e74a1
Denis Zakharenkov <zaharden@gmail.com>
2019-11-21 18:07:38 +0700

    .yarnrc fix (#1054)
    * CI init
    
    * Add decryption key for unique IDs only
    
    * Add encrypted .npmrc
    
    * Add encrypted .yarnrc

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 IMAGETAG=${BUILDKITE_BRANCH:-master}
 
 wget https://download.cyberway.io/commun/.npmrc.vault -O .npmrc
+wget https://download.cyberway.io/commun/.yarnrc.vault -O .yarnrc
 
 ansible-vault decrypt --vault-id=npmrc@~/commun-key .npmrc
+ansible-vault decrypt --vault-id=npmrc@~/commun-key .yarnrc
 
 docker build -t commun/commun:${IMAGETAG} .
 
-rm .npmrc
+rm .npmrc .yarnrc
 
 docker login -u=$DHUBU -p=$DHUBP
 docker push commun/commun:${IMAGETAG}
