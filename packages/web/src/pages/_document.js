5cc7d19565badcfae89218a5b378c3c9c04474b4
Alexey Elizarov <felex-ae@ya.ru>
2019-12-04 18:16:13 +0300

    feat: add analytics, fix google analytics (#1301)

--- packages/web/src/pages/_document.js
+++ packages/web/src/pages/_document.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-danger */
+/* eslint-disable react/no-danger,jsx-a11y/iframe-has-title */
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
@@ -7,13 +7,6 @@ import { pathOr } from 'ramda';
 import { GlobalStyles } from '@commun/ui';
 import { Sprite } from '@commun/icons';
 import { OG_DESCRIPTION, OG_NAME, OG_IMAGE, TWITTER_NAME } from 'shared/constants';
-
-const initGoogleAnalytics = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-0SLBQ9EP1H');
-`;
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -72,24 +65,56 @@ export default class MyDocument extends Document {
 
           <meta name="twitter:site" content={`@${TWITTER_NAME}`} />
 
-          {process.env.NODE_ENV === 'production' ? (
-            <>
-              <script async src="https://www.googletagmanager.com/gtag/js?id=G-0SLBQ9EP1H" />
-              <script
-                dangerousglySetInnerHTML={{
-                  __html: initGoogleAnalytics,
-                }}
-              />
-            </>
-          ) : null}
-
           <GlobalStyles />
           {this.props.styles}
 
           <script async defer src="https://platform.twitter.com/widgets.js" />
           <script async defer src="https://platform.instagram.com/en_US/embeds.js" />
+
+          {process.env.NODE_ENV === 'production' ? (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-0SLBQ9EP1H" />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-0SLBQ9EP1H');
+                  `,
+                }}
+              />
+
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PRBKH7M');
+                  `,
+                }}
+              />
+            </>
+          ) : null}
         </Head>
         <body>
+          {process.env.NODE_ENV === 'production' ? (
+            <noscript
+              dangerouslySetInnerHTML={{
+                __html: `
+                <iframe
+                  src="https://www.googletagmanager.com/ns.html?id=GTM-PRBKH7M"
+                  height="0"
+                  width="0"
+                  style="display:none;visibility:hidden;"
+                />
+              `,
+              }}
+            />
+          ) : null}
+
           <Sprite />
           <Main />
           <NextScript />
