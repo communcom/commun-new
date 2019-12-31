feb1fd5639e7521e2e0678a0d490899ba9856ac4
Alexey Elizarov <felex-ae@ya.ru>
2019-12-31 18:38:34 +0300

    feat: sign up auto show (#1491)

--- packages/web/src/components/common/OnboardingCheck/OnboardingCheck.connect.js
+++ packages/web/src/components/common/OnboardingCheck/OnboardingCheck.connect.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
 import { openModal } from 'redux-modals-manager';
 
-import { SHOW_MODAL_ONBOARDING_REGISTRATION } from 'store/constants';
-import { isAuthorizedSelector } from 'store/selectors/auth';
+import { SHOW_MODAL_ONBOARDING_REGISTRATION, SHOW_MODAL_SIGNUP } from 'store/constants';
+import { isUnsafeAuthorizedSelector } from 'store/selectors/auth';
+import { dataSelector } from 'store/selectors/common';
 
 import OnboardingCheck from './OnboardingCheck';
 
 export default connect(
   state => ({
-    isAuthorized: isAuthorizedSelector(state),
+    isAuthorized: isUnsafeAuthorizedSelector(state),
+    isAutoLogging: dataSelector(['auth', 'isAutoLogging'])(state),
   }),
   {
     openOnboardingRegistration: () => openModal(SHOW_MODAL_ONBOARDING_REGISTRATION),
+    openSignUpModal: () => openModal(SHOW_MODAL_SIGNUP),
   }
 )(OnboardingCheck);
--- packages/web/src/components/common/OnboardingCheck/OnboardingCheck.js
+++ packages/web/src/components/common/OnboardingCheck/OnboardingCheck.js
@@ -2,9 +2,17 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { ONBOARDING_REGISTRATION_WAIT_KEY } from 'shared/constants';
+import cookie from 'cookie';
 
-const OnboardingCheck = ({ isAuthorized, openOnboardingRegistration }) => {
+const OnboardingCheck = ({
+  isAuthorized,
+  isAutoLogging,
+  openOnboardingRegistration,
+  openSignUpModal,
+}) => {
   useEffect(() => {
+    const cookies = document.cookie ? cookie.parse(document.cookie) : {};
+
     if (isAuthorized && localStorage[ONBOARDING_REGISTRATION_WAIT_KEY]) {
       // TODO: Temporary disable check
       // openOnboardingRegistration();
@@ -14,14 +22,25 @@ const OnboardingCheck = ({ isAuthorized, openOnboardingRegistration }) => {
     if (isAuthorized && localStorage['onboarding.us']) {
       openOnboardingRegistration();
     }
-  }, [isAuthorized, openOnboardingRegistration]);
+
+    // eslint-disable-next-line dot-notation
+    if (!isAutoLogging && !isAuthorized && !cookies['commun_signup_proposed']) {
+      openSignUpModal();
+
+      // 6000000 === 10 minutes
+      const expires = new Date(Date.now() + 6000000).toGMTString();
+      document.cookie = `commun_signup_proposed=1; path=/; expires=${expires}`;
+    }
+  }, [isAuthorized, isAutoLogging, openOnboardingRegistration, openSignUpModal]);
 
   return null;
 };
 
 OnboardingCheck.propTypes = {
   isAuthorized: PropTypes.bool.isRequired,
+  isAutoLogging: PropTypes.bool.isRequired,
   openOnboardingRegistration: PropTypes.func.isRequired,
+  openSignUpModal: PropTypes.func.isRequired,
 };
 
 export default OnboardingCheck;
