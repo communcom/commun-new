78f5006101c7f1c3d13032bf1408bdc6a0066ad3
Ilya Lebedev <bacherko@gmail.com>
2020-01-29 16:46:26 +0300

    fix: improve reload page logic when script error occurs #1650

--- packages/web/src/utils/errorHandling.js
+++ packages/web/src/utils/errorHandling.js
@@ -1,6 +1,10 @@
 import Router from 'next/router';
+import { displayError } from 'utils/toastsMessages';
 
 if (process.browser) {
+  const LAST_RELOAD_KEY = 'system.lastReloadTry';
+  const RELOAD_TIME_LIMIT = 60 * 1000;
+
   let lastRouteUrl = null;
 
   Router.events.on('routeChangeStart', url => {
@@ -16,6 +20,15 @@ if (process.browser) {
       reason.code === 'PAGE_LOAD_ERROR' &&
       reason.message.startsWith('Error loading script')
     ) {
+      const dateString = window.localStorage.getItem(LAST_RELOAD_KEY);
+
+      // If we already have tried to reload page in RELOAD_TIME_LIMIT then just show error
+      if (dateString && new Date(dateString) > Date.now() - RELOAD_TIME_LIMIT) {
+        displayError(reason.message);
+        return;
+      }
+
+      window.localStorage.setItem(LAST_RELOAD_KEY, new Date().toJSON());
       window.location.assign(lastRouteUrl);
     }
   });
