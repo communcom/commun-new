292a8d9b072d105fd60e9fbd7b05f5da2a943c48
Alexey Elizarov <felex-ae@ya.ru>
2020-02-17 14:35:06 +0300

    feat: ff carbon show on HOST_ENV development (2)

--- packages/web/src/shared/featureFlags.js
+++ packages/web/src/shared/featureFlags.js
@@ -61,7 +61,7 @@ export default {
 
   // Wallet
   [FEATURE_EXCHANGE_COMMON]: true,
-  [FEATURE_EXCHANGE_CARBON]: false || process.env.HOST_ENV === 'development',
+  [FEATURE_EXCHANGE_CARBON]: false || process.env.WEB_HOST_ENV === 'development',
 
   // Registration
   [FEATURE_REGISTRATION_ALL]: true,
