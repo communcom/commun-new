a6ca3344e8430d1e70e118e9e9ca5d82b69cbcd3
Ilya Lebedev <bacherko@gmail.com>
2020-02-20 17:31:58 +0300

    fix: fix ios scroll restoring #1840

--- packages/web/src/utils/hooks/useWindowScrollLock.js
+++ packages/web/src/utils/hooks/useWindowScrollLock.js
@@ -1,10 +1,17 @@
 import { useLayoutEffect } from 'react';
 
+let scrollElement = null;
 let scrollLock = 0;
+let lastScrollTop = 0;
+
+if (process.browser) {
+  scrollElement = document.scrollingElement || document.documentElement;
+}
 
 export default function useWindowScrollLock() {
   useLayoutEffect(() => {
     if (scrollLock === 0) {
+      lastScrollTop = scrollElement.scrollTop;
       document.body.style.overflow = 'hidden';
     }
 
@@ -17,6 +24,10 @@ export default function useWindowScrollLock() {
 
       if (scrollLock === 0) {
         document.body.style.overflow = '';
+
+        if (scrollElement.scrollTop !== lastScrollTop) {
+          scrollElement.scrollTop = lastScrollTop;
+        }
       }
     };
   }, []);
